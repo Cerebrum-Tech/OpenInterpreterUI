736ac4f18b8e47888d1a90f2055f9d34d3477a7a
Alp Eren Özalp <alperenoz93@gmail.com>
2024-07-10 19:57:08 +0000

    chore: Update prompts.py with instructions for using data from an Excel file

--- src/utils/prompts.py
+++ src/utils/prompts.py
@@ -12,5 +12,13 @@ class PROMPTS:
         "Make sure you write the whole code in one cell and execute it.\n"
         "Do not stop until you finish the task.\n"
         "If you encounter any errors, you should handle them and re-run the code by fixing the error.\n"
+        "You have presented the data in the form of a excel file namet 'Cere Analytics Veri_son_22.12.2023.xlsx'.\n"
+        "For answering the questions, you should use the data in the excel file.\n"
+        "You can use the 'pandas' library to read the data.\n"
+        "You can use the 'matplotlib' library to visualize the data.\n"
+        "The data is in the 'Cere Analytics Veri_son' of the excel file.\n"
+        "The sheet has following columns: 'Tarih', 'Müşteri	İş - Ürün',	'Müşteri Bizi Nereden Buldu?',	'Fiyat Alınan Yer',	'İç İmalat / İç Üretim',	'Fason İmalat / Fason Üretim',	'Müşteri Grubu',	'Alınan Fiyat',	'Ek Maliyetler',	'Verilen Fiyat',	'Adet',	'Alış Tutarı',	'Alış KDV',	'Alış Tutarı', 'KDV'li	Satış Tutarı',	'Kar',	'KDV',	'Satış KDV',	'KDV Tahakkuku',	'KDV'li Tutar',	'Karlılık %',	'Vade',	'Ödeme Şekli',	'İşin Durumu'.\n"
+        "Always answer in Turkish.\n"
+        "You must answer in Turkish.\n"
         f"Today is {CURRENT_DATE_TIME}. \n"
     )
