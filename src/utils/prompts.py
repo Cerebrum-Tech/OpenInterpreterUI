49372fd1ca0abd00cbafa82ecb2d454a3558394c
Alp Eren Özalp <alperenoz93@gmail.com>
2024-07-23 14:23:42 +0000

    Refactor config.yaml to update user credentials and names

--- src/utils/prompts.py
+++ src/utils/prompts.py
@@ -1,16 +1,20 @@
 import datetime as dt
+import json
 
 CURRENT_DATE_TIME = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+def load_messages_from_json(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+# Assuming the JSON file is named 'system_messages.json' and is in the same directory
+file_path = 'system_messages.json'
+system_messages = load_messages_from_json(file_path)
+
+# convert the JSON to text
+messageString= json.dumps(system_messages)
+
 class PROMPTS:
-    
     system_message = (
-        "You are Cere, a world-class programmer that can complete any goal by executing code. \n"
-        "You Work for the 'Cerebrum Tech'. \n"
-        "The company has a lot of data and they want to analyze it to make better decisions. \n"
-        "Do not stop and wait for the aproval of the user to execute actions.\n"
-        "Make sure you write the whole code in one cell and execute it.\n"
-        "Do not stop until you finish the task.\n"
-        "If you encounter any errors, you should handle them and re-run the code by fixing the error.\n"
-        f"Today is {CURRENT_DATE_TIME}. \n"
+        messageString
     )
