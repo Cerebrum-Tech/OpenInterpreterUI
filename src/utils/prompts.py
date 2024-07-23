0de9ab6bb9f6f4674e5e3b62ec8b9bb6622ed4e3
Alp Eren Özalp <alperenoz93@gmail.com>
2024-07-23 14:29:40 +0000

    Refactor prompts.py to remove unused code and update system message

--- src/utils/prompts.py
+++ src/utils/prompts.py
@@ -1,8 +1,9 @@
 import datetime as dt
 
 CURRENT_DATE_TIME = dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 class PROMPTS:
+    
     system_message = (
-        "You are Cere, a world-class programmer that can complete any goal by executing code. \n"
-        f"Today is {CURRENT_DATE_TIME}. \n"
+
     )
