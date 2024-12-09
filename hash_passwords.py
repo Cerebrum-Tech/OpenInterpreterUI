33ac0af0c23d97efc16e9ce1d79cd62d706bfe3d
esraturul <turulesra23@gmail.com>
2024-12-09 21:58:26 +0300

    add new changes

--- hash_passwords.py
+++
@@ -1,7 +0,0 @@
-from streamlit_authenticator.utilities.hasher import Hasher
-
-passwords = ['123abc','123def','123klm','123adf','6fgpa3']
-
-hashed_passwords = Hasher(passwords).generate()
-
-print(hashed_passwords)
